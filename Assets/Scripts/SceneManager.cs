9533d982707d7071fcf25c8e6e981a08306b1162
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2020-04-09 16:35:16 +0800

    基礎設定

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour
+{
+    private Image imgcross;
+    private void Start()
+    {
+        
+    }
+    public IEnumerator NextLevel()
+    {
+        for (int k = 0; k < 50; k++)
+        {
+            imgcross.color += new Color(0, 0, 0, 0.03f);
+            yield return new WaitForSeconds(0.02f);
+        }
+
+
+       // SceneManager.LoadScene("第二關");
+        yield return new WaitForSeconds(1f);
+        for (int k = 0; k < 50; k++)
+        {
+            imgcross.color -= new Color(49, 49, 49, 0.03f);
+            yield return new WaitForSeconds(0.02f);
+        }
+
+    }
+}
