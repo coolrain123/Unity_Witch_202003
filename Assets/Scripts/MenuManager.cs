ab275c55f942a6cae361538ac99b3b57d3cdf8df
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2020-04-09 20:36:51 +0800

    小更新腳本

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("載入畫面")]
+    public GameObject panelLoading;
+    [Header("載入畫面文字")]
+    public Text textLoading;
+    [Header("載入畫面讀條")]
+    public Image imgLoading;
+
+
+
+    public void StartLoading()
+    {
+        print("開始載入....");
+        panelLoading.SetActive(true);
+        textLoading.text = "99 %";
+        imgLoading.fillAmount = 0.99f;
+        //SceneManager.LoadScene("關卡1");
+        StartCoroutine(Loading());
+    }
+
+    /// <summary>
+    /// 協程方法:載入
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator Loading()
+    {
+        print("載入中....");
+        yield return new WaitForSeconds(1);
+        print("載入中........123456");
+    }
+}
