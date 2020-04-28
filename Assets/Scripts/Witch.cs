3382c546f17d1ecf0b29fcbc103732b41a71996b
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2020-04-28 16:24:39 +0800

    女巫動作

---
+++ Assets/Scripts/Witch.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Witch : MonoBehaviour
+{
+    [Header("移動速度"), Range(1, 1000)]
+    public float speed = 5;
+    [Header("跳躍高度"), Range(1, 1000)]
+    public float jumpHeight = 5;
+
+    public Animator ani;
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        Move();
+       
+    }
+    public void Move()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            ani.SetBool("Walk", true);
+
+        }
+
+        float v = Input.GetAxis("Vertical");   //Vertical:WS上下
+        float h = Input.GetAxis("Horizontal"); //Horizontal:AD左右
+        transform.Translate(speed * Time.deltaTime * h, 0, 0);
+
+        //float joyv = joy.Vertical;
+        //float joyh = joy.Horizontal;
+        //transform.Translate(speed * Time.deltaTime * joyh, 0, speed * Time.deltaTime * joyv);
+
+        Vector2 pos = transform.position;    
+
+    }
+
+    public void Jump()
+    {
+        transform.Translate(0, jumpHeight, 0);
+        ani.SetTrigger("Jump");
+      
+    }
+
+}
